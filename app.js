874f10c5f3b05f972e0d61682a3519770d438e19
vishalBindal <vishalbindal.06@gmail.com>
2018-12-13 12:44:20 +0530

    scrolling effect added

--- app.js
+++ app.js
@@ -9,4 +9,20 @@ $('.js-form').submit(function(event){
 });
 
 
+$(document).ready(function () {
+     $('a[href^="#"]').on('click', function (e) {
+         e.preventDefault();
+
+         var target = this.hash,
+             $target = $(target);
+
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top 
+         }, 700, 'swing', function () {
+             window.location.hash = target;
+         });
+     });
+ });
+
+
 
